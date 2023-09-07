50f569ec547d7a9a04243afc12f0eac1e2b91ca5
Tal Halperin <tal9110@gmail.com>
2023-09-07 01:40:51 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,173 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Mindchattter from "./Mindchatter";
+import { Center, Image } from "@mantine/core";
+import {
+  createStyles,
+  Header,
+  Group,
+  ActionIcon,
+  Container,
+  rem,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import {
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconBrandInstagram,
+} from "@tabler/icons-react";
+import { useState } from "react";
+
+const useStyles = createStyles((theme) => ({
+  inner: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: rem(56),
+
+    [theme.fn.smallerThan("sm")]: {
+      justifyContent: "flex-start",
+    },
+  },
+
+  links: {
+    width: rem(300),
+
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  social: {
+    width: rem(260),
+
+    [theme.fn.smallerThan("sm")]: {
+      width: "auto",
+      marginLeft: "auto",
+    },
+  },
+
+  link: {
+    display: "block",
+    lineHeight: 0.8,
+    padding: `${rem(8)} ${rem(12)}`,
+    borderRadius: theme.radius.sm,
+    textDecoration: "none",
+    color: "white",
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    "&:hover": {
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[6]
+          : theme.colors.gray[7],
+    },
+  },
+
+  // linkActive: {
+  //   "&, &:hover": {
+  //     backgroundColor: theme.fn.variant({
+  //       variant: "light",
+  //       color: theme.colors.gray[7],
+  //     }).background,
+  //     color: theme.fn.variant({ variant: "light", color: theme.colors.gray[7] })
+  //       .color,
+  //   },
+  // },
+  linkActive: {
+    backgroundColor: theme.colors.dark[6],
+    "&, &:hover": {
+      color: "white",
+    },
+  },
+}));
 
 function App() {
+  const links = [
+    { label: "Home", link: "#home" },
+    { label: "About", link: "#about" },
+    { label: "Shows", link: "#shows" },
+    { label: "Merch", link: "#merch" },
+  ];
+  const [opened, { toggle }] = useDisclosure(false);
+  const [active, setActive] = useState(links[0].link);
+  const { classes, cx } = useStyles();
+
+  const items = links.map((link) => (
+    <a
+      key={link.label}
+      href={link.link}
+      className={cx(classes.link, {
+        [classes.linkActive]: active === link.link,
+      })}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(link.link);
+        if (link.label === "Merch") {
+          setModelInView("second");
+        } else {
+          setModelInView("first");
+        }
+      }}
+    >
+      {link.label}
+    </a>
+  ));
+
+  const [modelInView, setModelInView] = useState("first");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header
+        sx={{
+          position: "absolute",
+          zIndex: 1,
+          border: "none",
+          // top: "5%",
+          // left: "50%",
+          // transform: "translate(-50%, -50%)",
+          backgroundColor: "transparent",
+        }}
+        height={56}
+        // mb={120}
+      >
+        <Container className={classes.inner} style={{ maxWidth: "100%" }}>
+          <Group className={classes.links} spacing={5}>
+            {items}
+          </Group>
+
+          <Image src={"/mindlogo.png"} width={200} />
+          <Group spacing={2} className={classes.social} position="right" noWrap>
+            <ActionIcon variant="transparent" size="lg">
+              <IconBrandInstagram size="1.2rem" stroke={2} />
+            </ActionIcon>
+            <ActionIcon variant="transparent" size="lg">
+              <IconBrandTwitter size="1.2rem" stroke={2} />
+            </ActionIcon>
+            <ActionIcon variant="transparent" size="lg">
+              <IconBrandYoutube size="1.2rem" stroke={2} />
+            </ActionIcon>
+          </Group>
+        </Container>
+      </Header>
+      {/* <Center>
+        <Image
+          sx={{
+            position: "absolute",
+            zIndex: 1,
+            top: "5%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+          src={"/mindlogo.png"}
+          width={200}
+          // width={200}
+          // height={200}
+        />
+      </Center> */}
+      <Mindchattter modelInView={modelInView} />
+      {/* </div> */}
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+import { MantineProvider, Text } from "@mantine/core";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <App />
+      </MantineProvider>
+    </React.StrictMode>
+    {/* <div className="header">
+      <span className="active">ART</span>
+      <span>ABOUT</span>
+      <span>VISIT</span>
+      <span>SHOP</span>
+    </div> */}
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
